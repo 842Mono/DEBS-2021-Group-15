f60cedf384f1c2805e8a235b1696853c3766fc62
Baiqing Lyu <baiqing@bu.edu>
2021-03-04 10:00:40 -0500

    Added time calibration code, and finished grabbing data from API

--- API/gRPC-client/src/main/java/grpcClient.java
+++ API/gRPC-client/src/main/java/grpcClient.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.ManagedChannel;
 
+import com.grpc.Batch;
 import com.grpc.Benchmark;
 import com.grpc.BenchmarkConfiguration;
 import com.grpc.ChallengerGrpc;
+import com.grpc.Locations;
+import com.grpc.Ping;
+import com.grpc.ResultQ1;
+import com.grpc.ResultQ2;
 import com.grpc.ChallengerGrpc.ChallengerBlockingStub;
+import com.grpc.TopKStreaks;
+import com.grpc.TopKCities;
 public class grpcClient {
 
     public static void main(String[] args) {
@@ -12,7 +22,9 @@ public class grpcClient {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("challenge.msrg.in.tum.de", 5023).usePlaintext().build();
 
         // Create a blocking stub because we need to make sure this response is recieved
-        ChallengerBlockingStub userStub = ChallengerGrpc.newBlockingStub(channel);
+        ChallengerBlockingStub client = ChallengerGrpc.newBlockingStub(channel)
+            .withMaxInboundMessageSize(100 * 1024 * 1024)
+            .withMaxOutboundMessageSize(100 * 1024 * 1024);
 
         // Create a configuration object to be passed into the first set of creating a benchmark
         BenchmarkConfiguration benchmarkConfig = BenchmarkConfiguration.newBuilder()
@@ -20,10 +32,79 @@ public class grpcClient {
             .setBatchSize(100)
             .setBenchmarkName("group-15")
             .setBenchmarkType("test")
+            .addQueries(BenchmarkConfiguration.Query.Q1)
+            .addQueries(BenchmarkConfiguration.Query.Q2)
             .build();
 
         // Initiate step one and send over the benchmarkConfig
-        Benchmark benchmark = userStub.createNewBenchmark(benchmarkConfig);
-        System.out.println(benchmark.getId());
+        Benchmark benchmark = client.createNewBenchmark(benchmarkConfig);
+        System.out.println("Benchmark ID: " + benchmark.getId());
+        
+        // Get locations
+        Locations locations = client.getLocations(benchmark);
+        
+        // Start latency measuring
+        Ping ping = client.initializeLatencyMeasuring(benchmark);
+        for (int i = 0; i < 10; i++){
+            client.measure(ping);
+        }
+        client.endMeasurement(ping);
+
+        // Start benchmark, the race is on
+        client.startBenchmark(benchmark);
+        System.out.println("Bechmark Started!");
+
+        //Process the events
+        int cnt = 0;
+        while(true) {
+            Batch batch = client.nextBatch(benchmark);
+            if (batch.getLast()) { //Stop when we get the last batch
+                System.out.println("Received lastbatch, finished!");
+                break;
+            }
+
+            //process the batch of events we have
+            var topKImproved = calculateTopKImproved(batch);
+
+            ResultQ1 q1Result = ResultQ1.newBuilder()
+                    .setBenchmarkId(benchmark.getId())
+                    .setBatchSeqId(batch.getSeqId()) //set the sequence number
+                    .addAllTopkimproved(topKImproved)
+                    .build();
+
+            //return the result of Q1
+            client.resultQ1(q1Result);
+
+            var histogram = calculateHistogram(batch);
+            ResultQ2 q2Result = ResultQ2.newBuilder()
+                    .setBenchmarkId(benchmark.getId())
+                    .setBatchSeqId(batch.getSeqId()) //set the sequence number
+                    .addAllHistogram(histogram)
+                    .build();
+
+            client.resultQ2(q2Result);
+            System.out.println("Processed batch #" + cnt);
+            ++cnt;
+
+            if(cnt > 100) {
+                break;
+            }
+        }
+
+        client.endBenchmark(benchmark);
+        System.out.println("Ended Benchmark!");
+        
     }
+    private static List<TopKStreaks> calculateHistogram(Batch batch) {
+        //TODO: improve implementation
+
+        return new ArrayList<>();
+    }
+
+    private static List<TopKCities> calculateTopKImproved(Batch batch) {
+        //TODO: improve this implementation
+
+        return new ArrayList<>();
+    }
+
 }
